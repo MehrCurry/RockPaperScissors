61e4abc415c28e24c5822128e669a5e09e2363b8
Guido Zockoll <gzockoll@gmail.com>
2016-07-03 09:09:47 +0200

    suppress sonarqube warning

--- src/main/java/de/gzockoll/rps/boundary/GenericExceptionHandler.java
+++ src/main/java/de/gzockoll/rps/boundary/GenericExceptionHandler.java
@@ -10,14 +10,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-/**
- * Created by guido on 22.06.16.
- */
 @ControllerAdvice
 @Slf4j
 class GenericExceptionHandler {
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(IllegalArgumentException.class)
+    @SuppressWarnings("squid:S1172") // Unused method parameters should be removed
     public ModelAndView handleIllegalArgumentException(HttpServletRequest req, IllegalArgumentException ex) {
         if (AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class) != null)
             throw ex;
@@ -34,6 +32,7 @@ class GenericExceptionHandler {
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(IllegalStateException.class)
+    @SuppressWarnings("squid:S1172") // Unused method parameters should be removed
     public ModelAndView handleIllegalStateException(HttpServletRequest req, IllegalStateException ex) {
         if (AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class) != null)
             throw ex;
